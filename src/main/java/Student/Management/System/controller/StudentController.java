8d0d9b52bf12148ac1655e2aecfdb4cb33ce00eb
nivya <mahajannivya@gmail.com>
2026-07-23 12:06:22 +0530

    added mapping for add Student

--- src/main/java/Student/Management/System/controller/StudentController.java
+++ src/main/java/Student/Management/System/controller/StudentController.java
@@ -1,8 +1,11 @@
 package Student.Management.System.controller;
-
+import Student.Management.System.entity.Student;
 import Student.Management.System.service.StudentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("/students")
 @RestController
 public class StudentController {
     private final StudentService service;
@@ -10,4 +13,9 @@ public class StudentController {
     public StudentController(StudentService service) {
         this.service = service;
     }
+    @PostMapping("/add")
+    public void addStudent(@RequestBody Student student){
+        service.addStudent(student);
+    }
+
 }

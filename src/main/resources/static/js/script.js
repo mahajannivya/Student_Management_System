843d94685e892f6cf26141e835baa1ef23ef932d
nivya <mahajannivya@gmail.com>
2026-07-23 12:15:09 +0530

    updated with redirecting pages

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("add").addEventListener("click", function () {
+    window.location.href = "/html/addStudent.html";
+});
+
+document.getElementById("view").addEventListener("click", function () {
+    window.location.href = "/html/viewStudents.html";
+});
+
+document.getElementById("update").addEventListener("click", function () {
+    window.location.href = "/html/updateStudent.html";
+});
+
+document.getElementById("delete").addEventListener("click", function () {
+    window.location.href = "/html/deleteStudent.html";
+});
